397834644da983a9f7c3fddae8ea4ed0da95cfdc
ChaitanyaDevnikar <102657039+ChaitanyaDevnikar@users.noreply.github.com>
2022-11-16 10:41:57 +0530

    commit

---
+++ provider.tf
@@ -0,0 +1,19 @@
+#provider "aws" {
+#  region     = var.region
+#  access_key = var.access_key
+#  secret_key = var.secret_key
+#  }
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.region}"
+}
+
+resource "aws_instance" "web-server" {
+  ami           = "ami-0e6329e222e662a52"
+  instance_type = "t2.micro"
+
+#  tags {
+#    Name = "terraformtraining.com"
+#  }
+}
---
+++ s3.tf
@@ -0,0 +1,11 @@
+resource "aws_s3_bucket" "mybucket" {
+  bucket = "my-tff-buck"
+#  aws_s3_bucket_acl = "private"
+
+
+
+#  tags = {
+#    Name        = "rav1i-day1a-santhos1h-deepak1231"
+#    Environment = "Dev"
+#  }
+}
