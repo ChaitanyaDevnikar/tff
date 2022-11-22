21620ebc8b6f114eace18b214f81656534e35c32
ChaitanyaDevnikar <102657039+ChaitanyaDevnikar@users.noreply.github.com>
2022-11-22 12:25:23 +0530

    Update

--- vpc.tf
+++ vpc.tf
@@ -44,3 +44,20 @@ resource "aws_route_table" "Pu_RT" {
 resource "aws_route_table" "Private-RT" {
   vpc_id = aws_vpc.demo.id
   }
+
+
+resource "aws_instance" "nginx1" {
+  ami                    = data.aws_ssm_parameter.ami.value
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.subnet1.id
+  vpc_security_group_ids = [aws_security_group.nginx-sg.id]
+key_name = "mohnish"
+    user_data = <<EOF
+        #! /bin/bash
+        sudo yum update -y
+        sudo yum install -y docker
+        sudo service docker start
+        sudo usermod -a -G docker ec2-user
+        sudo docker pull chaitany3/new1
+        sudo docker run -p 8081:8081 --name terra -d chaitany3/new1
+    EOF
