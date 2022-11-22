0975d63fa3d987916333310a219a38b3a4b8a98c
ChaitanyaDevnikar <102657039+ChaitanyaDevnikar@users.noreply.github.com>
2022-11-22 15:49:13 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,10 +3,10 @@
 #  access_key = var.access_key
 #  secret_key = var.secret_key
 #  }
-provider "aws" {
-  access_key = "${var.AWS_ACCESS_KEY_ID}"
-  secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
-  region     = "${var.region}"
+provider "aws"{
+    access_key = var.AWS_ACCESS_KEY_ID
+    secret_key = var.AWS_SECRET_ACCESS_KEY
+    region = var.aws_region
 }
 
 resource "aws_instance" "web-server" {
